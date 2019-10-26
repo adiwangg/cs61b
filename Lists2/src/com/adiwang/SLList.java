8347c9bd362d74a4fb1255fa6d476b2450865d93
Adiwang <645337972@qq.com>
2019-10-26 21:59:52 +0800

    improvement: sentinal node

--- Lists2/src/com/adiwang/SLList.java
+++ Lists2/src/com/adiwang/SLList.java
@@ -13,31 +13,31 @@ public class SLList {
 		}
 	}
 	
-	private IntNode first;
+	private IntNode sentinal;
 	private int size;
-	
-	public SLList(int x) {
-		first = new IntNode(x,null);
-		size = 1;
-	}
-	
+
 	public SLList() {
-		first = null;
+		sentinal = new IntNode(66,null);
 		size = 0;
+	}
+
+	public SLList(int x) {
+		sentinal = new IntNode(66,null);
+		sentinal.next = new IntNode(x,null);
+		size = 1;
 	}
 	
 	
 	/** Adds x to the front of the list */
 	public void addFirst(int x) {
-		first = new IntNode(x,first);
+		sentinal.next = new IntNode(x,sentinal.next);
 		size++;
 	}
-	
+
 	/** Adds x to the end of the list */
 	public void addLast(int x) {
-		
-		if(first!=null) {
-			IntNode p = first;
+
+			IntNode p = sentinal;
 			
 			/* Move p until it reaches the end of the list */
 			while(p.next!=null) {
@@ -45,29 +45,23 @@ public class SLList {
 			}
 			p.next = new IntNode(x,null);
 			size++;
-		}else {
-			addFirst(x);
-		}
 		
 	}
 	
 	/** Retrieves the front item from the list */
 	public int getFirst() {
-		return first.item;
+		return sentinal.next.item;
 	}
 	
 	public int size() {
 		return size;
 	}
-
 	
 	public static void main(String[] args) {
 		SLList L = new SLList();
 		L.addLast(20);
 		
 		System.out.println(L.size());
-		System.out.println("updated from win 2!");
 
 	}
-
 }
