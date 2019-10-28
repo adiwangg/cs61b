d6d260476445249627695a13a2c1827efd526493
Dylan Huang <645337972@qq.com>
2019-10-28 16:51:04 +0800

    start pro0

---
+++ Pro0/src/com/adiwang/Planet.java
@@ -0,0 +1,30 @@
+package com.adiwang;
+
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;   //Its current velocity in the x direction
+    public double yyVel;   //Its current velocity in the y direction
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    /** Initialize an identical Planet object (copy) */
+    public Planet(Planet p){
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+}
