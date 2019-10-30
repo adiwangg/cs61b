70888aca993553a85936bcb71129963a7c661384
Dylan Huang <645337972@qq.com>
2019-10-30 10:40:30 +0800

    start to draw background

--- Pro0/src/NBody.java
+++ Pro0/src/NBody.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class NBody {
 
@@ -9,42 +10,51 @@ public class NBody {
         return R;
     }
 
-    public static int calcNumOfPlanets(String dir){
-        In in = new In(dir);
-        int num = 0;
-
-        while (in.hasNextLine()){
-            num += 0;
-        }
-        return (num - 2);
-    }
-
-//    public static Planet[] readPlanets(String dir){
+//    public static int calcNumOfPlanets(String dir){
 //        In in = new In(dir);
 //        int num = 0;
 //
-//        int numOfPlanets = in.readInt();
-//        double r = in.readDouble();
-//
-//        while(in.hasNextLine()){
-//            num += 1;
-//            double xP = in.readDouble();
-//            double yP = in.readDouble();
-//            double xV = in.readDouble();
-//            double yV = in.readDouble();
-//            double m = in.readDouble();
-//            String img = in.readString();
-//
+//        while (in.hasNextLine()){
+//            num += 0;
 //        }
-//
-//        Planet[] planets = new Planet[num];
-//
+//        return (num - 2);
 //    }
 
+    public static Planet[] readPlanets(String dir){
+        In in = new In(dir);
+
+        int numOfPlanets = in.readInt();
+        double r = in.readDouble();
+        Planet[] planets = new Planet[numOfPlanets];
+
+        for(int i = 0; i < numOfPlanets; i++){
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            planets[i] = new Planet(xP,yP,xV,yV,m,img);
+        }
+
+        return planets;
+    }
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the T:");
+        double T = sc.nextDouble();
+        System.out.println("Please enter the dt:");
+        double dt = sc.nextDouble();
+        System.out.println("Please enter the filename:");
+        String filename = sc.nextLine();
+
         String dir = "./data/planets.txt";
-        NBody n = new NBody();
-        System.out.println(n.readRadius(dir));
-        System.out.println(n.calcNumOfPlanets(dir));
+        double r = NBody.readRadius(dir);
+        Planet[] planets = NBody.readPlanets(dir);
+
+
+
     }
 }
