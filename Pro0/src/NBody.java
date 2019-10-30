8516618bde624f887d06433acfd2f120fadda38b
Adiwang <645337972@qq.com>
2019-10-30 08:42:25 +0800

    update

--- Pro0/src/NBody.java
+++ Pro0/src/NBody.java
@@ -4,14 +4,47 @@ public class NBody {
     public static double readRadius(String dir) {
         In in = new In(dir);
 
-        int N = in.readInt();
+        int numOfPlanets = in.readInt();
         double R = in.readDouble();
         return R;
     }
 
-//    public static void main(String[] args) {
-//        String dir = "planets.txt";
-//        NBody n = new NBody();
-//        System.out.println(n.readRadius(dir));
+    public static int calcNumOfPlanets(String dir){
+        In in = new In(dir);
+        int num = 0;
+
+        while (in.hasNextLine()){
+            num += 0;
+        }
+        return (num - 2);
+    }
+
+//    public static Planet[] readPlanets(String dir){
+//        In in = new In(dir);
+//        int num = 0;
+//
+//        int numOfPlanets = in.readInt();
+//        double r = in.readDouble();
+//
+//        while(in.hasNextLine()){
+//            num += 1;
+//            double xP = in.readDouble();
+//            double yP = in.readDouble();
+//            double xV = in.readDouble();
+//            double yV = in.readDouble();
+//            double m = in.readDouble();
+//            String img = in.readString();
+//
+//        }
+//
+//        Planet[] planets = new Planet[num];
+//
 //    }
+
+    public static void main(String[] args) {
+        String dir = "./data/planets.txt";
+        NBody n = new NBody();
+        System.out.println(n.readRadius(dir));
+        System.out.println(n.calcNumOfPlanets(dir));
+    }
 }
