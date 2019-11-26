1e8140a291bf61028d533bf2d3d599c89ae78e7a
Adiwang <645337972@qq.com>
2019-11-26 21:26:41 +0800

    clab8 task1 completed

--- lab/clab8/FlightSolver.java
+++ lab/clab8/FlightSolver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -12,17 +13,39 @@ import java.util.PriorityQueue;
 
 public class FlightSolver {
 
-    private PriorityQueue<Flight> pq;
+    private int maxNumPeople = 0;
 
-    // Constructor that takes in an ArrayList of Flight objects as described above.
     public FlightSolver(ArrayList<Flight> flights) {
         /* FIX ME */
+        PriorityQueue<Flight> startMinPQ = new PriorityQueue<>(new Comparator<Flight>() {
+            @Override
+            public int compare(Flight o1, Flight o2) {
+                return Integer.compare(o1.startTime(), o2.startTime());
+            }
+        });
+        PriorityQueue<Flight> endMinPQ = new PriorityQueue<>(new Comparator<Flight>() {
+            @Override
+            public int compare(Flight o1, Flight o2) {
+                return Integer.compare(o1.endTime(), o2.endTime());
+            }
+        });
+        startMinPQ.addAll(flights);
+        endMinPQ.addAll(flights);
+
+        int tally = 0;
+        while (startMinPQ.size() != 0) {
+            if (startMinPQ.peek().startTime() <= endMinPQ.peek().endTime()) {
+                tally += startMinPQ.poll().passengers();
+                maxNumPeople = tally > maxNumPeople ? tally : maxNumPeople;
+            } else {
+                tally -= endMinPQ.poll().passengers();
+            }
+        }
     }
 
-    // Returns the solution to the problem described above.
     public int solve() {
         /* FIX ME */
-        return -1;
+        return maxNumPeople;
     }
 
 }
