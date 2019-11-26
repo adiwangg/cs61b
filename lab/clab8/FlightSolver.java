15116f38386e8d23c60c0f0c7d83095bd45306bc
Adiwang <645337972@qq.com>
2019-11-26 20:46:53 +0800

    Release clab8

---
+++ lab/clab8/FlightSolver.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+/**
+ * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
+ * Assumes valid input, i.e. all Flight start times are >= end times.
+ * If a flight starts at the same time as a flight's end time, they are
+ * considered to be in the air at the same time.
+ */
+
+// Implement an algorithm for finding the largest number of people that have ever been in flight at once
+
+public class FlightSolver {
+
+    private PriorityQueue<Flight> pq;
+
+    // Constructor that takes in an ArrayList of Flight objects as described above.
+    public FlightSolver(ArrayList<Flight> flights) {
+        /* FIX ME */
+    }
+
+    // Returns the solution to the problem described above.
+    public int solve() {
+        /* FIX ME */
+        return -1;
+    }
+
+}
